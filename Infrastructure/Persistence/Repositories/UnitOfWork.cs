1ff8d25942c4f44457ebc05f5851a2773a10207d
GHK <gamalhanafi26@gmail.com>
2025-04-17 08:54:25 +0200

    Unit of Work

---
+++ Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class UnitOfWork(StoreDbContext _dbContext) : IUnitOfWork
+    {
+        private readonly Dictionary<string,object> _repositories = [];
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            var TypeName= typeof(TEntity).Name;
+            if (_repositories.TryGetValue(TypeName,out object? value))
+                return (IGenericRepository<TEntity,TKey>) value;
+
+            else
+            {
+                var Repo = new GenericRepositoy<TEntity, TKey>(_dbContext);
+                _repositories[TypeName]=Repo;
+                return Repo;
+            }
+            
+        }
+
+        public async Task<int> SaveChangesAsync() => await _dbContext.SaveChangesAsync();
+    }
+}
