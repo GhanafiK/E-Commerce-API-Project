ffb1271ed086c912e8d1ff4e96496471c2f7079d
GHK <gamalhanafi26@gmail.com>
2025-04-17 17:32:05 +0200

    Applying Auto Mapper

---
+++ Core/Service/MappingProfiles/ProductProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using DomainLayer.Models;
+using Shared.DataTransfareObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.MappingProfiles
+{
+    public class ProductProfile:Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDTO>().
+                ForMember(dest => dest.BrandName, Options => 
+                Options.MapFrom(src => src.productBrand.Name))
+                .ForMember(dest => dest.TypeName, Options =>
+                Options.MapFrom(src => src.productType.Name));
+
+            CreateMap<ProductBrand, BrandDTO>();
+            CreateMap<ProductType, TypeDTO>();
+
+        }
+    }
+}
