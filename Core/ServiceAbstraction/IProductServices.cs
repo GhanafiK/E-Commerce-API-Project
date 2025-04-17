30dcfe5289875adabca0d15d4c7bd7fcc3d1b34b
GHK <gamalhanafi26@gmail.com>
2025-04-17 09:33:37 +0200

    Product Module - Product Services

---
+++ Core/ServiceAbstraction/IProductServices.cs
@@ -0,0 +1,17 @@
+﻿using Shared.DataTransfareObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAbstraction
+{
+    public interface IProductServices
+    {
+        Task<IEnumerable<ProductDTO>> GetAllProductsAsync();
+        Task<IEnumerable<BrandDTO>> GetAllBrandsAsync();
+        Task<IEnumerable<TypeDTO>> GetAllTypesAsync();
+        Task<ProductDTO> GetProductByIdAsync(int id);
+    }
+}
