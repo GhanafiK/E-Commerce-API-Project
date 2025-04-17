4b5708d14d10d6f9cba335fa8dbbcd07cf6ef946
GHK <gamalhanafi26@gmail.com>
2025-04-17 21:31:38 +0200

    Product Controller

--- Infrastructure/Presentation/Controllers/ProductsController.cs
+++ Infrastructure/Presentation/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using ServiceAbstraction;
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransfareObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,38 @@ using System.Threading.Tasks;
 
 namespace Presentation.Controllers
 {
-    public class ProductsController(IServiceManager _serviceManager)
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductsController(IServiceManager _serviceManager):ControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetAllProducts()
+        {
+            var Products = await _serviceManager.ProductServices.GetAllProductsAsync();
+            return Ok(Products);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductDTO>> GetById(int id)
+        {
+            var Product=await _serviceManager.ProductServices.GetProductByIdAsync(id);  
+            return Ok(Product);
+        }
+
+        [HttpGet("Brands")]
+        public async Task<ActionResult<IEnumerable<BrandDTO>>> GetAllBrands()
+        {
+            var Brands=await _serviceManager.ProductServices.GetAllBrandsAsync();
+            return Ok(Brands);
+        }
+
+
+        [HttpGet("Types")]
+        public async Task<ActionResult<IEnumerable<TypeDTO>>> GetAllTpes()
+        {
+            var Types=await _serviceManager.ProductServices.GetAllTypesAsync();
+            return Ok(Types);
+        }
+
     }
 }
