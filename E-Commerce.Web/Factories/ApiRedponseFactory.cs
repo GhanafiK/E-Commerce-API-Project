8c2e279b0d97b2f40d499205a767e16da1892309
GHK <gamalhanafi26@gmail.com>
2025-04-24 09:09:32 +0200

    Validation Error Response

---
+++ E-Commerce.Web/Factories/ApiRedponseFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.ErrorModels;
+
+namespace E_Commerce.Web.Factories
+{
+    public static class ApiResponseFactory
+    {
+        public static IActionResult GenerateApiValidationErrorsResponse(ActionContext context)
+        {
+            var Errors = context.ModelState.Where(M => M.Value.Errors.Any())
+                    .Select(m =>
+                        new ValidationError()
+                        {
+                            Field = m.Key,
+                            Errors = m.Value.Errors.Select(E => E.ErrorMessage)
+                        });
+            var Response = new ValidationErrorToReturn()
+            {
+                ValidationErrors = Errors
+            };
+            return new BadRequestObjectResult(Response);
+        }
+    }
+}
