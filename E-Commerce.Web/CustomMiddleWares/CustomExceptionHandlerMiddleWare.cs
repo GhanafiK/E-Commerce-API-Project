a6150632bae49ae65a1244a847102a70a66ba198
GHK <gamalhanafi26@gmail.com>
2025-04-24 07:50:33 +0200

    Not Found EndPoint

--- E-Commerce.Web/CustomMiddleWares/CustomExceptionHandlerMiddleWare.cs
+++ E-Commerce.Web/CustomMiddleWares/CustomExceptionHandlerMiddleWare.cs
@@ -21,33 +21,54 @@ namespace E_Commerce.Web.CustomMiddleWares
             try
             {
                 await next.Invoke(httpContext);
-            }catch (Exception ex)
+                await HandleNotFoundEndPointAsync(httpContext);
+            }
+            catch (Exception ex)
             {
-                // set status code for Response
-                //httpContext.Response.StatusCode = 500;
-                //httpContext.Response.StatusCode=(int) HttpStatusCode.InternalServerError;
-                httpContext.Response.StatusCode = ex switch
+                logger.LogError(ex, "Something went wrong");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            // set status code for Response
+            //httpContext.Response.StatusCode = 500;
+            //httpContext.Response.StatusCode=(int) HttpStatusCode.InternalServerError;
+            httpContext.Response.StatusCode = ex switch
+            {
+                NotFoundException => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            // set Content Type For Response
+            //httpContext.Response.ContentType = "application/json";
+
+            // Response object
+            var Response = new ErrorToReturn
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                ErrorMessage = ex.Message
+            };
+
+            // return object as json
+            //var ResponseToReturn=JsonSerializer.Serialize(Response);
+            //await httpContext.Response.WriteAsync(ResponseToReturn);
+
+            await httpContext.Response.WriteAsJsonAsync(Response); // this make ContentType = "application/json"
+                                                                   // make json serialization and write async
+        }
+
+        private static async Task HandleNotFoundEndPointAsync(HttpContext httpContext)
+        {
+            if (httpContext.Response.StatusCode == StatusCodes.Status404NotFound)
+            {
+                var Response = new ErrorToReturn()
                 {
-                    NotFoundException => StatusCodes.Status404NotFound,
-                    _ =>StatusCodes.Status500InternalServerError
+                    StatusCode = StatusCodes.Status404NotFound,
+                    ErrorMessage = $"EndPoint => {httpContext.Request.Path} is not found"
                 };
-
-                // set Content Type For Response
-                //httpContext.Response.ContentType = "application/json";
-
-                // Response object
-                var Response = new ErrorToReturn
-                {
-                    StatusCode = httpContext.Response.StatusCode,
-                    ErrorMessage = ex.Message
-                };
-
-                // return object as json
-                //var ResponseToReturn=JsonSerializer.Serialize(Response);
-                //await httpContext.Response.WriteAsync(ResponseToReturn);
-
-                await httpContext.Response.WriteAsJsonAsync(Response); // this make ContentType = "application/json"
-                                                                       // make json serialization and write async
+                await httpContext.Response.WriteAsJsonAsync(Response);
             }
         }
     }
