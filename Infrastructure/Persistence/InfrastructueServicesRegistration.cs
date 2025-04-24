3d61d7e065de915bd89d5d5a7310786f00eee1ea
GHK <gamalhanafi26@gmail.com>
2025-04-24 21:37:41 +0200

    Basket Module - Repository

--- Infrastructure/Persistence/InfrastructueServicesRegistration.cs
+++ Infrastructure/Persistence/InfrastructueServicesRegistration.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Data;
 using Persistence.Repositories;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,12 @@ namespace Persistence
             Services.AddScoped<IDataSeeding, DataSeeding>();
             Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+            Services.AddScoped<IBasketRepository, BasketRepository>();
+
+            Services.AddSingleton<IConnectionMultiplexer>((_) =>
+            {
+                return ConnectionMultiplexer.Connect(Configuration.GetConnectionString("RedisConnectionString"));
+            });
             return Services;
         }
     }
