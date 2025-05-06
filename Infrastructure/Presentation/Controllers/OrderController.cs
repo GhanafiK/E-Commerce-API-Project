661ef80e06bf78c7832fad1d69822f177ac91016
GHK <gamalhanafi26@gmail.com>
2025-05-06 18:41:03 +0300

    Order Controller

--- Infrastructure/Presentation/Controllers/OrderController.cs
+++ Infrastructure/Presentation/Controllers/OrderController.cs
@@ -11,15 +11,41 @@ using System.Threading.Tasks;
 
 namespace Presentation.Controllers
 {
+    [Authorize]
     public class OrdersController(IServiceManager _serviceManager):ApiBaseController
     {
         //create Order
-        [Authorize]
         [HttpPost]
         public async Task<ActionResult<OrderToReturnDto>> CreateOrder(OrderDto orderDto)
         {
             var Order= await _serviceManager.OrderService.CreateOrderAsync(orderDto,GetEmailFromToken());
             return Ok(Order);
         }
+
+        // Get Delivery Methods
+        [AllowAnonymous]
+        [HttpGet("DeliveryMethods")]
+        public async Task<ActionResult<IEnumerable<DeliveryMethodDto>>> GetDeliveryMethods()
+        {
+            var DeliveryMethods= await _serviceManager.OrderService.GetDeliveryMethodsAsync();
+            return Ok(DeliveryMethods);
+        }
+
+        // Get All Orders By Email
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderToReturnDto>>> GetAllOrders()
+        {
+            var Orders = await _serviceManager.OrderService.GetAllOrderAsync(GetEmailFromToken());
+            return Ok(Orders);
+        }
+
+        //Get Order By Id
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderById(Guid id)
+        {
+            var order=await _serviceManager.OrderService.GetOrderByIdAsync(id);
+            return Ok(order);
+        }
+
     }
 }
